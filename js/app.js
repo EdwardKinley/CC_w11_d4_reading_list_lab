2fe886355dec4b3e1ddc729faa537054463f6450
EdwardKinley <edward_kinley@yahoo.co.uk>
2018-08-02 15:43:59 +0100

    puts books in flexible boxes

--- js/app.js
+++ js/app.js
@@ -9,8 +9,19 @@ document.addEventListener('DOMContentLoaded', () => {
     const author = event.target.author.value;
     const category = event.target.category.value;
 
-    const newBook = document.createElement("p");
-    newBook.textContent = `Author: ${author}; title: ${title}; category: ${category}.`;
+    const newBook = document.createElement("ul");
+    const newBookAuthor = document.createElement("li");
+    const newBookTitle = document.createElement("li");
+    const newBookCategory = document.createElement("li");
+
+    newBookAuthor.textContent = `Author: ${author}`;
+    newBookTitle.textContent = `Title: ${title}`;
+    newBookCategory.textContent = `Category: ${category}`;
+    newBook.appendChild(newBookAuthor);
+    newBook.appendChild(newBookTitle);
+    newBook.appendChild(newBookCategory);
+
+
 
     const readingList = document.querySelector('#reading-list');
     readingList.appendChild(newBook);
